a30db0723e791f83bd3e88be08ccabbde38d2964
xiter22222 <pawanpediredla@gmail.com>
2024-11-29 08:10:27 +0530

    Add function to handle payment creation using Stripe

---
+++ .history/controllers/paymentControllers_20241129080839.js
@@ -0,0 +1,20 @@
+/*
+  This code belongs to Pawvan.
+  It is part of the stripe_api_integration_wrapper project.
+  Licensed under MIT.
+*/
+
+const {createPaymentIntent} =require('../services/stripeServices')
+
+const createPayment  = async (req,res,next)=>{
+try{
+const {amount,money} =  req.body
+
+
+const paymentIntent  = await 
+}
+catch(error){
+
+}
+
+}
---
+++ .history/controllers/paymentControllers_20241129080858.js
@@ -0,0 +1,21 @@
+/*
+  This code belongs to Pawvan.
+  It is part of the stripe_api_integration_wrapper project.
+  Licensed under MIT.
+*/
+
+const {createPaymentIntent} =require('../services/stripeServices')
+
+const createPayment  = async (req,res,next)=>{
+try{
+const {amount,money} =  req.body
+
+
+const paymentIntent  = await createPaymentIntent(amount,currency)
+res.status(200)
+}
+catch(error){
+
+}
+
+}
