b5851706272f9664eaac0e576ef489ce6c214ae2
xiter22222 <pawanpediredla@gmail.com>
2024-11-29 08:03:53 +0530

    added config/stripeConfig.js

---
+++ .history/config/stripeConfig_20241129080324.js
@@ -0,0 +1,13 @@
+
+
+/*
+  This code belongs to Pawvan.
+  It is part of the stripe_api_integration_wrapper project.
+  Licensed under MIT.
+*/
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
+
+
+
+
+
---
+++ utils/logger.js
@@ -0,0 +1,9 @@
+/*
+  This code belongs to Pawvan.
+  It is part of the stripe_api_integration_wrapper project.
+  Licensed under MIT.
+*/
+
+
+
+
